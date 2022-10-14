fef103c102dc53ac799f4500b92448dff0e60f0a
bbbozhena <bbbozhenka@gmail.com>
2022-10-14 17:28:53 +0300

    add email value

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 "use strict";
 
+const btn = document.getElementById("button");
+const emailInput = document.getElementById("exampleInputEmail1");
+
+const buttonClick = btn.addEventListener("click", () => {
+  return openConfirm();
+});
+
+function openConfirm() {
+  const emailData = emailInput.value;
+
+ window.confirm(`Please confirm account creation for ${emailData} `) 
+    if (confirm === true) {
+       
+      } else {
+       
+      }
 
 
+}
