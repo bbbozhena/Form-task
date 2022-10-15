0250416d6eeafe92c363ad4739b7f20992fb6c77
bbbozhena <bbbozhenka@gmail.com>
2022-10-15 11:30:59 +0300

    add true confirm

--- script.js
+++ script.js
@@ -2,20 +2,28 @@
 
 const btn = document.getElementById("button");
 const emailInput = document.getElementById("exampleInputEmail1");
+const form = document.getElementById("form-container");
+const element = document.createElement("div");
+const image = element.innerHTML = `  <img src="/style/img/kot-nyashka.jpeg" > `
 
-const buttonClick = btn.addEventListener("click", () => {
+const buttonClick = btn.addEventListener("click", (e) => {
   return openConfirm();
+  e.preventDefault();
 });
 
 function openConfirm() {
   const emailData = emailInput.value;
+  const ask = window.confirm(
+    `Please confirm account creation for ${emailData} `
+  );
 
- window.confirm(`Please confirm account creation for ${emailData} `) 
-    if (confirm === true) {
-       
-      } else {
-       
-      }
-
-
+  if (ask) {
+    document.body.innerHTML = `<div class="windo"> <p> Hello user with email: ${emailData} </p>
+    ${image}
+    </div>
+    `
+    ;
+  } else {
+    console.log("neok");
+  }
 }
