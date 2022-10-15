21c11ccabc9696f460195c097ed4afc334d5cd9f
bbbozhena <bbbozhenka@gmail.com>
2022-10-15 20:59:25 +0300

    add email pattern

--- script.js
+++ script.js
@@ -2,28 +2,42 @@
 
 const btn = document.getElementById("button");
 const emailInput = document.getElementById("exampleInputEmail1");
+const passwordInput = document.getElementById("exampleInputPassword1");
 const form = document.getElementById("form-container");
 const element = document.createElement("div");
-const image = element.innerHTML = `  <img src="/style/img/kot-nyashka.jpeg" > `
+const image =
+  (element.innerHTML = `  <img src="./style/img/kot-nyashka.png" > `);
 
-const buttonClick = btn.addEventListener("click", (e) => {
-  return openConfirm();
-  e.preventDefault();
+function checkEmail(emailInput) {
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  if (re.test(emailInput.value.trim())) {
+    openConfirm();
+  } else {
+    console.log("email is not valid");
+  }
+}
+
+function checkForm() {
+  if (emailInput.value == "" || passwordInput.value == "") {
+    alert("Please enter the data ");
+  }
+}
+
+const buttonClick = btn.addEventListener("click", () => {
+  checkEmail(emailInput);
+  return checkForm();
 });
 
 function openConfirm() {
   const emailData = emailInput.value;
   const ask = window.confirm(
-    `Please confirm account creation for ${emailData} `
+    `Please confirm account creation for: ${emailData} `
   );
 
   if (ask) {
     document.body.innerHTML = `<div class="windo"> <p> Hello user with email: ${emailData} </p>
-    ${image}
-    </div>
-    `
-    ;
-  } else {
-    console.log("neok");
+        ${image}
+        </div>
+        `;
   }
 }
